a5be7f79f35575d9a0915f956dec839c903976c1
crazycluboriginal <65145858+crazycluboriginal@users.noreply.github.com>
2025-07-15 03:55:23 +0000

    update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ process.traceDeprecation = true;
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 const port = process.env.PORT || 8080;
 
 // Set EJS as view engine
